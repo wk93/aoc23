8c67511b88923eb2591ae231783812b93069cee1
Wojciech Kania <wojtek@kania.sh>
2023-12-06 09:44:56 +0100

    day 6, puzzle 1

---
+++ src/day6.rs
@@ -0,0 +1,64 @@
+use std::fs;
+pub fn puzzle1() {
+    let races = parse_input(get_input(false));
+
+    let mut multiple: usize = 1;
+    for race in races {
+        let mut options: Vec<i32> = Vec::new();
+        let duration = race.0;
+        let record = &race.1;
+        for x in 0..duration {
+            options.push(x * (duration - x));
+        }
+        let sum = options.iter().filter(|distance| distance > &record).count();
+
+        multiple *= sum;
+    }
+
+    println!("{}", multiple);
+}
+
+fn parse_input(input: Vec<String>) -> Vec<(i32, i32)> {
+    let mut races: Vec<(i32, i32)> = Vec::new();
+
+    for line in input {
+        let nums = line.split(":").collect::<Vec<&str>>()[1]
+            .split(" ")
+            .filter(|x| !x.is_empty())
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        for (i, num) in nums.iter().enumerate() {
+            match races.get_mut(i) {
+                Some(race) => {
+                    race.1 = num.to_owned();
+                }
+                None => {
+                    races.push((num.to_owned(), 0));
+                }
+            }
+        }
+    }
+
+    races
+}
+
+fn get_input(example: bool) -> Vec<String> {
+    match example {
+        true => vec![
+            "Time:      7  15   30".to_string(),
+            "Distance:  9  40  200".to_string(),
+        ],
+        false => {
+            let filename = "static/day6-input.txt";
+
+            let mut v = Vec::new();
+
+            for line in fs::read_to_string(filename).unwrap().lines() {
+                v.push(line.to_string());
+            }
+
+            v
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     println!("Hello, world!");
-    day5::puzzle1();
-    day5::puzzle2();
+    day6::puzzle1();
 }
